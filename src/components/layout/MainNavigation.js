6b56f3012e7750d60e99bf8f3b16396aec9a13d6
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2022-01-17 21:16:44 +0300

    initial commit

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import classes from './MainNavigation.module.scss';
+
+const MainNavigation = () => {
+  return <div className={classes.main}>Navigation</div>;
+};
+
+export default MainNavigation;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Home.module.scss';
+
+const Home = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Figma Ecommerce</p>
+      </header>
+    </div>
+  );
+};
+
+export default Home;
