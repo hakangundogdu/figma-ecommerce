efaf129ac3ea297869354982b230f8338f2fa20d
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2022-03-01 14:39:29 +0300

    link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../components/ui/Button';
 import classes from './Home.module.scss';
-import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -14,12 +13,8 @@ const Home = () => {
           <br />
           Figma. You can even sync designs from your cloud storage!
         </p>
-<<<<<<< HEAD
         <Link to="/Shop">
           {' '}
-=======
-        <Link to="Shop">
->>>>>>> 8d2849ebd6b52d069fc6336dbeda3b3f39be6f99
           <Button className={classes.button} name="Shop All" />
         </Link>
       </header>
